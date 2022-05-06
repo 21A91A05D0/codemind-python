52ccd045de4549d9b6ebd09b1833569de3bf52b5
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-05-06 09:43:18 +0530

    s=input() l=len(s) for i in range(1,l+1):     if s[0]==0:         print('Invalid')     elif l==10:         print('Valid')         break     else:         print('Invalid')         break

---
+++ PHONE_NUMBER_VALIDATION:.py
@@ -0,0 +1,11 @@
+s=input()
+l=len(s)
+for i in range(1,l+1):
+    if s[0]==0:
+        print('Invalid')
+    elif l==10:
+        print('Valid')
+        break
+    else:
+        print('Invalid')
+        break
