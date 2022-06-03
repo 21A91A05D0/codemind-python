996eeaf0c18f52c134ac741ff3053172b9aaff22
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-06-03 11:08:24 +0530

    from math import pow n=int(input()) i=0 r=0 while n>0:     d=n%10     r+=d*pow(8,i)     n=n//10     i+=1 l,j=0,0 while r>0:     d=r%2     l+=d*pow(10,j)     r=r//2     j+=1 print(int(l))

---
+++ octal_to_binary.py
@@ -0,0 +1,18 @@
+from math import pow
+n=int(input())
+i=0
+r=0
+while n>0:
+    d=n%10
+    r+=d*pow(8,i)
+    n=n//10
+    i+=1
+l,j=0,0
+while r>0:
+    d=r%2
+    l+=d*pow(10,j)
+    r=r//2
+    j+=1
+print(int(l))
+
+    
