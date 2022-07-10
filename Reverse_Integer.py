134a23eac608180c14f36d9fbbe0da1831101645
Eathala Praveen Pothan <97424509+21A91A05D0@users.noreply.github.com>
2022-07-11 00:12:03 +0530

    n=int(input()) r,f=0,0 if n<0:     f=1     n=abs(n) while n:     d=n%10     r=r*10+d     n=n//10 if f==0:     print(r) else:     print('-%d' %r)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,14 +1,13 @@
 n=int(input())
-rev=0
-f=0
+r,f=0,0
 if n<0:
     f=1
     n=abs(n)
-while(n):
+while n:
     d=n%10
-    rev=rev*10+d
+    r=r*10+d
     n=n//10
 if f==0:
-    print(rev)
+    print(r)
 else:
-    print("-%d" %rev)
+    print('-%d' %r)
